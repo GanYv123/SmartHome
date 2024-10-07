f5e94ab83ef2bdf2ae2173d49bbc2a3419111098
xvyang zhao <105866682+GanYv123@users.noreply.github.com>
2024-10-07 21:43:06 +0800

    Add files via upload

---
+++ SmartHome/main.cpp
@@ -0,0 +1,17 @@
+#include "dialog.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+
+    /* 兼容高分辨率屏幕 */ //zxy commit 1
+#if (QT_VERSION > QT_VERSION_CHECK(5,6,0))
+    QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+
+    QApplication a(argc, argv);
+    Dialog w;
+    w.show();
+    return a.exec();
+}
